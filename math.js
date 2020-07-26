77b9f35e25bca6be00f19ef9a0c798f972f68980
LikithaVenna <68274754+LikithaVenna@users.noreply.github.com>
2020-07-26 19:00:50 +0530

    Create html and js files to show math. functions

---
+++ math.js
@@ -0,0 +1,50 @@
+'use strict'
+let x=27.09
+
+console.log('Returns the value of the number x rounded to the nearest integer.')
+console.log(Math.round(x))
+
+console.log('Returns a pseudo-random number between 0 and 1')
+console.log(Math.random(x))
+
+console.log('Returns the smallest integer greater than or equal to x.')
+console.log(Math.ceil(x))  
+
+console.log('Returns the sign of the x, indicating whether x is positive, negative, or zero.')
+console.log(Math.sign(x))
+
+console.log('Returns the sine of x')
+console.log(Math.sin(x))
+
+console.log('Returns the largest integer less than or equal to x')
+console.log(Math.floor(x))
+
+console.log('Returns the nearest single precision float representation of x.')
+console.log(Math.fround(x))
+
+let y=64
+
+console.log('Returns the positive square root of x')
+console.log(Math.sqrt(y))
+
+console.log('Returns base a to the exponent power b (that is, a^b)')
+let a = 2
+let b=5
+console.log(Math.pow(a,b))
+
+console.log('Return pi value')
+console.log(Math.PI)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
